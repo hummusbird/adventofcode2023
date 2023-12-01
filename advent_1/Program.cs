83a231863a26b1c29be11af34e24869325870fbe
hummusbird <birb@birb.cc>
2023-12-01 10:41:23 +0000

    advent 1

---
+++ advent_1/Program.cs
@@ -0,0 +1,43 @@
+﻿namespace advent_1;
+class Program
+{
+    static void Main(string[] args)
+    {
+        string[] instructions = File.ReadAllText("input.txt").Split("\n");
+
+        List<int> calibrations = new();
+
+        foreach (string line in instructions)
+        {
+            for (int i = 0; i < line.Length; i++)
+            {
+                if (int.TryParse(line[i].ToString(), out int value))
+                {
+                    //Console.WriteLine(value);
+                    calibrations.Add(value * 10);
+                    break;
+                }
+            }
+
+            for (int i = line.Length; i >= 0; i--)
+            {
+                if (int.TryParse(line[i - 1].ToString(), out int value))
+                {
+                    //Console.WriteLine(value);
+                    calibrations[^1] += value;
+                    break;
+                }
+            }
+        }
+
+        int total = 0;
+
+        foreach (int num in calibrations)
+        {
+            Console.WriteLine(num);
+            total += num;
+        }
+
+        Console.WriteLine("Total: " + total);
+    }
+}
---
+++ advent_1_2/Program.cs
@@ -0,0 +1,76 @@
+﻿namespace advent_1_2;
+class Program
+{
+    static void Main(string[] args)
+    {
+        string[] instructions = File.ReadAllText("input.txt").Split("\n");
+
+        string[] numbers = { "one", "two", "three", "four", "five", "six", "seven", "eight", "nine" };
+
+        List<int> calibrations = new();
+
+        foreach (string line in instructions)
+        {
+            for (int i = 0; i < line.Length; i++)
+            {
+                bool foundfirst = false;
+
+                if (int.TryParse(line[i].ToString(), out int value))
+                {
+                    calibrations.Add(value * 10);
+                    break;
+                }
+
+                foreach (string num in numbers)
+                {
+                    if (line.Length - i + 1 - num.Length > 0) // prevent going out of range when matching substrings
+                    {
+                        if (line.Substring(i, num.Length) == num) // try and match every number to each substring of its length
+                        {
+                            calibrations.Add((Array.IndexOf(numbers, num) + 1) * 10);
+                            foundfirst = true;
+                            break;
+                        }
+                    }
+                }
+
+                if (foundfirst) { break; }
+            }
+
+            for (int i = line.Length - 1; i >= 0; i--)
+            {
+                bool foundlast = false;
+
+                if (int.TryParse(line[i].ToString(), out int value))
+                {
+                    calibrations[^1] += value;
+                    break;
+                }
+
+                foreach (string num in numbers)
+                {
+                    if (line.Length - i + 1 - num.Length > 0) // prevent going out of range when matching substrings
+                    {
+                        if (line.Substring(i, num.Length) == num) // try and match every number to each substring of its length, in reverse
+                        {
+                            calibrations[^1] += Array.IndexOf(numbers, num) + 1;
+                            foundlast = true;
+                            break;
+                        }
+                    }
+                }
+                if (foundlast) { break; }
+            }
+        }
+
+        int total = 0;
+
+        foreach (int num in calibrations)
+        {
+            Console.WriteLine(num);
+            total += num;
+        }
+
+        Console.WriteLine("Total: " + total);
+    }
+}
