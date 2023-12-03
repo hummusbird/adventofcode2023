9cec8bf2f74bfd2f16b78c698d79266d382b747d
hummusbird <birb@birb.cc>
2023-12-03 20:59:53 +0000

    try to simplify advent_3 to find the bug

--- advent_3/Program.cs
+++ advent_3/Program.cs
@@ -3,7 +3,7 @@ class Program
 {
     static void Main(string[] args)
     {
-        string[] input = File.ReadAllLines("test.txt");
+        string[] input = File.ReadAllLines("input.txt");
 
         int total = 0;
 
@@ -13,7 +13,7 @@ class Program
             {
                 int length = 0;
 
-                while ((x < input[y].Length) && input[y][x] >= '0' && input[y][x] <= '9') // check for numbers and count their length
+                while (x < input[y].Length && char.IsDigit(input[y][x])) // check for numbers and count their length
                 {
                     length++;
                     x++;
@@ -28,29 +28,29 @@ class Program
                     for (int k = -1; k <= 1; k++) // k is our y incrementor
                     {
                         if (y == 0 && k < 0) { k++; } // don't go out of range
-                        else if (y + k == input.Count() || found) { break; } // don't go out of range
+                        else if (y + k == input.Length || found) { break; } // don't go out of range
 
                         for (int j = -1; j <= length + 1; j++) // j is our x incrementor
                         {
                             if (x == 0 && j < 0) { j++; } // don't go out of range
-                            else if (x + j == input[y].Count() || found) { break; } // dont go out of range
+                            else if (x + j == input[y].Length || found) { break; } // dont go out of range
 
                             //Console.WriteLine("length:\t" + length + "\nx: " + x + " y: " + y + "\nj: " + j + " k: " + k);
 
-                            if (((input[y + k][x + j] < '0') || (input[y + k][x + j] > '9')) && input[y + k][x + j] != '.') // anything that isn't a number or '.'
+                            if (!char.IsDigit(input[y + k][x + j]) && input[y + k][x + j] != '.') // anything that isn't a number or '.'
                             {
                                 found = true;
-                                int number = 0;
+                                string number = "";
 
                                 for (int h = 0; h < length; h++)
                                 {
-                                    number += (int)(int.Parse(input[y][x + h].ToString()) * Math.Pow(10, length - h - 1)); // we create the number like this: 4*100 + 6*10 + 1*1 = 461
+                                    number += input[y][x + h].ToString();
                                 }
 
                                 Console.WriteLine("number:\t" + number);
                                 //Console.WriteLine("\nsymbol:\t" + input[y + k][x + j]);
                                 //Console.WriteLine("coords:\t" + (y + k + 1) + "," + (x + j + 1));
-                                total += number; // add number to total
+                                total += int.Parse(number); // add number to total
                                 break;
                             }
                         }
