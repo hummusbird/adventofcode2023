f5a32205cf2805922500a3e16de1e01ee00d9ab2
hummusbird <birb@birb.cc>
2023-12-06 09:13:17 +0000

    advent_6

---
+++ advent_6/Program.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections.Immutable;
+
+namespace advent_6;
+class Program
+{
+    static void Main(string[] args)
+    {
+        List<string> input = File.ReadAllLines("input.txt").ToList();
+
+        List<ulong> time = input[0].Replace("Time: ", "").Split(" ").Select(ulong.Parse).ToList();
+        List<ulong> distance = input[1].Replace("Distance: ", "").Split(" ").Select(ulong.Parse).ToList();
+
+        List<ulong> waystowin = new();
+
+        for (int i = 0; i < time.Count; i++)
+        {
+            waystowin.Add(0);
+
+            for (ulong j = 0; j < time[i]; j++)
+            {
+                ulong dist = (time[i] - j) * j;
+                if (dist > distance[i]) { waystowin[i]++; }
+            }
+        }
+
+        ulong score = 1;
+
+        foreach (ulong i in waystowin)
+        {
+            score *= i;
+        }
+
+        Console.WriteLine(score);
+    }
+}
