d92e9f1233849a7a9a7e39e1e71e334eb9f34432
hummusbird <birb@birb.cc>
2023-12-08 13:28:37 +0000

    advent 8

---
+++ advent_8/Program.cs
@@ -0,0 +1,27 @@
+﻿namespace advent_8;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        string input = File.ReadAllText("input.txt");
+
+        List<string> node_text = input.Split("\n\n").Last().Split("\n").ToList();
+        string instructions = input.Split("\n\n").First();
+
+        int steps = 0;
+        string node = "AAA";
+
+        while (true)
+        {
+            int direction = instructions[steps % instructions.Length].ToString() == "R" ? 1 : 0;
+
+            node = node_text.Find(x => x.StartsWith(node)).Replace(" ", "").Substring(5, 7).Split(",")[direction];
+
+            steps++;
+            if (node == "ZZZ") { break; }
+        }
+
+        Console.WriteLine(steps);
+    }
+}
