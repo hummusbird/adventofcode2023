d87d78d9054200aee8739997f09f2a15f36de227
hummusbird <birb@birb.cc>
2023-12-15 09:17:30 +0000

    advent 14 and advent 10

---
+++ advent_10/Program.cs
@@ -0,0 +1,20 @@
+﻿namespace advent_10;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        List<string> lines = File.ReadAllLines("test2.txt").ToList();
+        ulong length = 0;
+        //Y , X
+        (int, int) S_location = (lines.IndexOf(lines.Find(x => x.Contains('S'))), lines.Find(x => x.Contains('S')).IndexOf('S'));
+        (int, int) current = S_location.ToTuple().ToValueTuple();
+
+        while (true)
+        {
+
+
+
+        }
+    }
+}
---
+++ advent_14/Program.cs
@@ -0,0 +1,39 @@
+﻿namespace advent_14;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        List<string> input = File.ReadAllLines("input.txt").ToList();
+
+        for (int y = 1; y < input.Count; y++)
+        {
+            int cur_Y = y;
+            for (int x = 0; x < input[cur_Y].Length; x++)
+            {
+                while (input[cur_Y][x] == 'O' && cur_Y > 0 && input[cur_Y - 1][x] == '.')
+                {
+                    char[] lastline = input[cur_Y - 1].ToCharArray();
+                    lastline[x] = 'O';
+                    input[cur_Y - 1] = new string(lastline);
+
+                    char[] currline = input[cur_Y].ToCharArray();
+                    currline[x] = '.';
+                    input[cur_Y] = new string(currline);
+
+                    cur_Y--;
+                }
+                cur_Y = y;
+            }
+        }
+
+        int load = 0;
+
+        for (int i = 0; i < input.Count; i++)
+        {
+            load += input[i].Count(c => c == 'O') * (input.Count - i);
+        }
+
+        Console.WriteLine(load);
+    }
+}
