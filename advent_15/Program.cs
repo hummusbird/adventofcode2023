8261fdaf951ca19f571e9ff3fc808ec8a0d1a673
hummusbird <birb@birb.cc>
2023-12-15 09:26:53 +0000

    advent 15

---
+++ advent_15/Program.cs
@@ -0,0 +1,25 @@
+﻿namespace advent_15;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        List<string> strings = File.ReadAllText("input.txt").Split(",").ToList();
+        int sum = 0;
+
+        foreach (string step in strings)
+        {
+            int value = 0;
+            foreach (char character in step)
+            {
+                value += character;
+                value *= 17;
+                value %= 256;
+            }
+
+            sum += value;
+        }
+
+        Console.WriteLine(sum);
+    }
+}
