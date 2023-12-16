ea2307524dc330051aa3823a1841b7bf6576a899
hummusbird <birb@birb.cc>
2023-12-16 18:08:20 +0000

    advent 10

--- advent_10/Program.cs
+++ advent_10/Program.cs
@@ -2,19 +2,34 @@
 
 class Program
 {
+    static public List<string> lines = File.ReadAllLines("input.txt").ToList();
+
     static void Main(string[] args)
     {
-        List<string> lines = File.ReadAllLines("test2.txt").ToList();
-        ulong length = 0;
         //Y , X
         (int, int) S_location = (lines.IndexOf(lines.Find(x => x.Contains('S'))), lines.Find(x => x.Contains('S')).IndexOf('S'));
         (int, int) current = S_location.ToTuple().ToValueTuple();
+        current.Item1--; // start at the item above S, as S is actually J
+        (int, int) last = current.ToTuple().ToValueTuple();
 
-        while (true)
-        {
+        Console.WriteLine(findnextpipe(current, last, S_location, 1) / 2);
+    }
 
+    static long findnextpipe((int, int) current, (int, int) last, (int, int) S_location, long length)
+    {
+        (int, int) next = current.ToTuple().ToValueTuple();
 
+        if (lines[current.Item1][current.Item2] == '|') { if (last.Item1 == current.Item1 + 1) { next.Item1--; } else { next.Item1++; } }
+        else if (lines[current.Item1][current.Item2] == '-') { if (last.Item2 == current.Item2 + 1) { next.Item2--; } else { next.Item2++; } }
+        else if (lines[current.Item1][current.Item2] == 'L') { if (last.Item1 == current.Item1 - 1) { next.Item2++; } else { next.Item1--; } }
+        else if (lines[current.Item1][current.Item2] == 'J') { if (last.Item1 == current.Item1 - 1) { next.Item2--; } else { next.Item1--; } }
+        else if (lines[current.Item1][current.Item2] == '7') { if (last.Item2 == current.Item2 - 1) { next.Item1++; } else { next.Item2--; } }
+        else if (lines[current.Item1][current.Item2] == 'F') { if (last.Item2 == current.Item2 + 1) { next.Item1++; } else { next.Item2++; } }
 
-        }
+        length++;
+
+        if (current == S_location) { return length; }
+
+        return findnextpipe(next, current, S_location, length);
     }
 }
