19d0b052519cb5163961e50e3df984760d703652
hummusbird <birb@birb.cc>
2024-10-06 17:59:39 +0100

    advent_11 advent_16

---
+++ advent_11/Program.cs
@@ -0,0 +1,9 @@
+﻿namespace advent_11;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Hello, World!");
+    }
+}
---
+++ advent_16/Program.cs
@@ -0,0 +1,20 @@
+﻿namespace advent_16;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        List<string> input = File.ReadAllLines("test.txt").ToList();
+        List<string> board = input.ToList();
+
+
+
+        for (int y = 0; y < board.Count; y++)
+        {
+            for (int x = 0; x < board[y].Length; x++)
+            {
+
+            }
+        }
+    }
+}
